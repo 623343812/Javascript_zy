3ac6f47fddd681fbcfd524cb7f22ca6d03e6fcda
623343812 <623343812@qq.com>
2020-08-03 22:47:50 +0800

    :eagle: nodejs03!

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// jQuery = function (agr) {
+//     alert(agr);
+// }
+// $ = window.jQuery = jQuery
+
+// $(agr)
+// jQuery(agr)
+
+// $('div').addClass('div');
+// jQuery('div').addClass('div');
+
+/* $(document).ready(function() {
+    $('div').addClass('div');
+}); */  
+
+/* $().ready(function() {
+    $('div').addClass('div');
+}); */
+
+/* $(function() {
+    $('div').addClass('div');
+}); */
